5083eeb8480c194f557b7867b5b56cee0a9a7ea9
Xiaoxuan Wang <xiaoxuanwang@Xiaoxuans-MacBook-Pro.local>
2020-04-06 22:04:21 -0700

    Complete tasks

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,34 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const wordSearch = (letters, word) => {
+  const transpose = function (matrix) {
+    const result = [];
+    for (let i = 0; i < matrix[0].length; i++) {
+      let row = [];
+      for (let j = 0; j < matrix.length; j++) {
+        row.push(matrix[j][i]);
+      }
+      result.push(row);
     }
-}
+    return result;
+  };
 
-module.exports = wordSearch
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+
+  let horizonCheck = false;
+
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      horizonCheck = true;
+    }
+  }
+
+  let verticalCheck = false;
+  const verticalJoin = transpose(letters).map((ls) => ls.join(""));
+  for (l of verticalJoin) {
+    if (l.includes(word)) {
+      verticalCheck = true;
+    }
+  }
+  return horizonCheck || verticalCheck;
+};
+
+module.exports = wordSearch;
